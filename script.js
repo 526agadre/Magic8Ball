1555ccecbd1f860a7eff08433c994b3f4c480623
526agadre <526agadre@frhsd.com>
2024-12-06 10:24:14 -0500

    a friend helped me out

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let userName = prompt("Greetings, whats is your name?")
+let userName = prompt("whats your name?")
 //Input
 
 let responseIMG = document.getElementById('response-img')
@@ -6,11 +6,11 @@ let responseText = document.getElementById('response-text')
 let greeting = document.getElementById('greeting')
 
 userName ? (greeting.innerText= `Hello, ${userName}!` )
-: (greeting.innerText= 'Hello Stranger')
+: (greeting.innerText= 'Hello boy')
 
 
 function shakeMagic8Ball(){
-    let userQuestion = prompt("What Do You Want To Ask The Bear?")
+    let userQuestion = prompt("What do you want to ask?")
     console.log(userQuestion);
     
     //DO NOT TOUCH
@@ -21,46 +21,46 @@ function shakeMagic8Ball(){
     switch (randomNumber) {
       case 0:
           eightBall = 'Yes';
-          imageChoice = 'IMGS/FrontFacingDjungelskog.png'
+          imageChoice = 'yes.png'
         break;
       case 1:  
         eightBall = 'It is certain';
-        imageChoice = 'IMGS/FrontFacingDjungelskog.png'
+        imageChoice = '8ball.png'
         break;
       case 2:
           eightBall = 'It is decidedly so';
-          imageChoice = 'IMGS/FrontFacingDjungelskog.png'
+          imageChoice = 'yes.png'
         break;
       case 3:
           eightBall = 'Your speaking is incoherent, reply again';
-          imageChoice = 'IMGS/FrontFacingDjungelskog.png'
+          imageChoice = 'thumbs down.png'
         break;
       case 4:
           eightBall = 'Cannot predict now';
-          imageChoice = 'IMGS/FrontFacingDjungelskog.png'
+          imageChoice = 'no.png'
         break;
       case 5:
           eightBall = 'Unlikely';
-          imageChoice = 'IMGS/FrontFacingDjungelskog.png'
+          imageChoice = 'no.png'
         break;
       case 6:
           eightBall = 'No';
-          imageChoice = 'IMGS/FrontFacingDjungelskog.png'
+          imageChoice = 'no.png'
         break;
       case 7:
           eightBall = 'It is Beyond My Understanding';
-          imageChoice = 'IMGS/FrontFacingDjungelskog.png'
+          imageChoice = 'yes.png'
         break;
       case 8:
         eightBall = 'It Seems To Be So';
-        imageChoice = 'IMGS/FrontFacingDjungelskog.png'
+        imageChoice = 'yes.png'
         break;
         
     }
     
     console.log(eightBall)
 
-   responseText.innerText = `${userName} Has Asked ${userQuestion} To The Almighty Djungelskog. The Bear Replies ${eightBall}!`
+   responseText.innerText = `${userName} Has Asked ${userQuestion} to the boy. The boy Replies ${eightBall}!`
 
    responseIMG.src = imageChoice
 }
