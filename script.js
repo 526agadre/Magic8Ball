a64ffffd5205c9c30ab2d15d5231e79e9e003e69
526agadre <526agadre@frhsd.com>
2024-12-06 10:15:32 -0500

    this is golden

--- script.js
+++ script.js
@@ -1,38 +1,66 @@
-//Get user name
-let userName = prompt('Hello what is your name');
-//Imput user name to website
-userName ? document.getElementById('greeting').innerText=`Hello, ${userName}!`: document.getElementById('greeting').innerText = 'Hello Stranger'
-function shakeMagic8Ball(){
-    //Get user question
-    let userQuestion = 'Will I be successful today?';
+let userName = prompt("Greetings, whats is your name?")
+//Input
 
-    console.log(`${userName ? userName : 'The user'} asked: ${userQuestion}`);
-   
-    //Do not touch
-    let randomNumber = Math.floor(Math.random() * 8);
-   
-   
+let responseIMG = document.getElementById('response-img')
+let responseText = document.getElementById('response-text')
+let greeting = document.getElementById('greeting')
+
+userName ? (greeting.innerText= `Hello, ${userName}!` )
+: (greeting.innerText= 'Hello Stranger')
+
+
+function shakeMagic8Ball(){
+    let userQuestion = prompt("What Do You Want To Ask The Bear?")
+    console.log(userQuestion);
+    
+    //DO NOT TOUCH
+    let randomNumber = Math.floor(Math.random()* 8);
     let eightBall = '';
-   
-    if (randomNumber === 0) {
-      eightBall = 'It is certain';
-    } else if (randomNumber === 1) {
-      eightBall = 'It is decidedly so';
-    } else if (randomNumber === 2) {
-      eightBall = 'Reply hazy, try again';
-    } else if (randomNumber === 3) {
-      eightBall = 'Cannot predict now';
-    } else if (randomNumber === 4) {
-      eightBall = 'Do not count on it';
-    } else if (randomNumber === 5) {
-      eightBall = 'My sources say no';
-    } else if (randomNumber === 6) {
-      eightBall = 'Outlook not so good';
-    } else if (randomNumber === 7) {
-      eightBall = 'Signs point to yes';
+    let imageChoice = ''
+    
+    switch (randomNumber) {
+      case 0:
+          eightBall = 'Yes';
+          imageChoice = 'IMGS/FrontFacingDjungelskog.png'
+        break;
+      case 1:  
+        eightBall = 'It is certain';
+        imageChoice = 'IMGS/FrontFacingDjungelskog.png'
+        break;
+      case 2:
+          eightBall = 'It is decidedly so';
+          imageChoice = 'IMGS/FrontFacingDjungelskog.png'
+        break;
+      case 3:
+          eightBall = 'Your speaking is incoherent, reply again';
+          imageChoice = 'IMGS/FrontFacingDjungelskog.png'
+        break;
+      case 4:
+          eightBall = 'Cannot predict now';
+          imageChoice = 'IMGS/FrontFacingDjungelskog.png'
+        break;
+      case 5:
+          eightBall = 'Unlikely';
+          imageChoice = 'IMGS/FrontFacingDjungelskog.png'
+        break;
+      case 6:
+          eightBall = 'No';
+          imageChoice = 'IMGS/FrontFacingDjungelskog.png'
+        break;
+      case 7:
+          eightBall = 'It is Beyond My Understanding';
+          imageChoice = 'IMGS/FrontFacingDjungelskog.png'
+        break;
+      case 8:
+        eightBall = 'It Seems To Be So';
+        imageChoice = 'IMGS/FrontFacingDjungelskog.png'
+        break;
+        
     }
-   
-   
-    console.log(`The Magic Eight Ball says: ${eightBall}`);
-   
+    
+    console.log(eightBall)
+
+   responseText.innerText = `${userName} Has Asked ${userQuestion} To The Almighty Djungelskog. The Bear Replies ${eightBall}!`
+
+   responseIMG.src = imageChoice
 }
